1d37da04caf7d3debded0f6d1e80d1e0a0b43def
Minn01 <149309870+Minn01@users.noreply.github.com>
2024-02-16 14:28:36 +0700

    Update StandardFrame.java

--- StandardFrame.java
+++ StandardFrame.java
@@ -2,14 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public interface StandardFrame {
-
+    // Adding a predetermined size that we want to start of initially
     static Dimension frameSize = new Dimension(1200, 800);
+    // Getting the screen size of any device that is using to run this
     static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    // Get Methods of the width and height
     static int screenWidth = screenSize.width;
-    static int screenHeight = screenSize.width;
+    static int screenHeight = screenSize.height;
 
+    // Makes it so that the frame opens in the upper middle location of your screen 
     static void setFrameMiddle(JFrame frame) {
+        // Getting the middle position of the device's screen
         int middleScreenPosition = (screenSize.width - frame.getSize().width) / 2;
+        // And then set location to that position
         frame.setLocation(middleScreenPosition, 0);
     }
 
