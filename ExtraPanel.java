fced34c71d0c4773859e78db92d74d6def9c38b4
Minn01 <149309870+Minn01@users.noreply.github.com>
2024-02-28 21:14:24 +0700

    Update ExtraPanel.java

--- ExtraPanel.java
+++ ExtraPanel.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
+// A simple class used to create invisible panels to use for the grid layout
 public class ExtraPanel extends JPanel {
     public ExtraPanel() {
         setOpaque(false);
