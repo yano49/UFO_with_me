eba881b77fd9ba42469a3a96c3d744b1b6f325f3
Minn01 <149309870+Minn01@users.noreply.github.com>
2024-02-28 21:09:01 +0700

    Update BlackHole.java

--- BlackHole.java
+++ BlackHole.java
@@ -1,9 +1,11 @@
+// Subclass of the abstract Projectile class
 public class BlackHole extends Projectile {
 
     public BlackHole(int x, int y) {
         super(x, y);
     }
 
+    // Abstract method of projectile class
     @Override
     public void move() {
         x -= 2;
