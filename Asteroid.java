a1f4ec1989a255084788740e8541de110cdbb526
Minn01 <149309870+Minn01@users.noreply.github.com>
2024-02-28 21:05:42 +0700

    Update Asteroid.java
    Commented

--- Asteroid.java
+++ Asteroid.java
@@ -1,4 +1,6 @@
+// Subclass of abstract class projectile
 public class Asteroid extends Projectile{
+    // Connected to the game panel
     private GamePanel gamePanel;
 
     public Asteroid(double x, double y, GamePanel gamePanel) {
@@ -6,6 +8,7 @@ public class Asteroid extends Projectile{
         this.gamePanel = gamePanel;
     }
 
+    // Implementing the abstract method
     @Override
     public void move() {
         x -= gamePanel.getProjectileSpeed();
